6c908a635aefae0a7ebd3ec106ba87da137ebea9
Bryan Herbst <bryan.c.herbst@gmail.com>
2021-03-13 13:03:38 -0600

    Run spotless

--- app/src/main/java/com/example/androiddevchallenge/WelcomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/WelcomeScreen.kt
@@ -1,12 +1,37 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
-import android.widget.Space
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +41,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import dev.chrisbanes.accompanist.insets.statusBarsPadding
 
 @Composable
 fun WelcomeScreen(
@@ -67,7 +91,7 @@ fun WelcomeScreen(
                 OutlinedButton(
                     modifier = Modifier.fillMaxSize()
                         .weight(1f),
-                    onClick = { navigateToLogin()},
+                    onClick = { navigateToLogin() },
                     colors = ButtonDefaults.outlinedButtonColors(
                         backgroundColor = Color.Transparent,
 
@@ -78,7 +102,6 @@ fun WelcomeScreen(
                     Text(text = "LOG IN")
                 }
             }
-
         }
     }
 }
